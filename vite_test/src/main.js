b1794911432137ed0d44dbfe942f4ac13b15914d
yKanon <394769450@qq.com>
2022-03-20 15:23:03 +0800

    first commit

---
+++ vite_test/src/main.js
@@ -0,0 +1,10 @@
+import _ from 'lodash-es';
+// import { join } from '../node_modules/@types/lodash-es/index.d.ts';
+// import _ from '../node_modules/lodash-es/lodash.default.js';
+import add from './js/math';
+
+console.log('hello world!');
+console.log(add(20, 30));
+console.log(_.join(['123', '1123'], '-'));
+// _.join([]);
+// join();
