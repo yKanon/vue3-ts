74da6d9c1ebfbb73c87b542cadb4e39473e3b5ec
yKanon <394769450@qq.com>
2022-07-27 15:57:43 +0800

    feat: 处理props

--- mini_vue3/renderer.js
+++ mini_vue3/renderer.js
@@ -37,3 +37,37 @@ const mount = (vnode, container) => {
 
   container.appendChild(el);
 };
+
+const patch = (n1, n2) => {
+  if (n1.tag !== n2.tag) {
+    const n1ParentEl = n1.el.parentElement;
+    n1ParentEl.removeChild(n1.el);
+    mount(n2, n1ParentEl);
+  } else {
+    // 处理props
+    let el = (n2.el = n1.el);
+
+    for (const key in n2.props) {
+      const oldValue = n1.props[key];
+      const newValue = n2.props[key];
+
+      if (oldValue !== newValue) {
+        if (key.startsWith('on')) {
+          el.addEventListener(key.slice(2).toLowerCase(), newValue);
+        } else {
+          el.setAttribute(key, newValue);
+        }
+      }
+    }
+
+    for (const key in n1.props) {
+      if (!(key in n2.props)) {
+        if (key.startsWith('on')) {
+          el.removeEventListener(key.slice(2).toLowerCase(), n1.props[key]);
+        } else {
+          el.removeAttribute(key, oldValue);
+        }
+      }
+    }
+  }
+};
